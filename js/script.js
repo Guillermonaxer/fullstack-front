80c2da9c890cb68141f84dee6a6a41cad7b389cd
Guillermonaxer <105644228+Guillermonaxer@users.noreply.github.com>
2022-06-06 13:11:28 +0200

    Añado disponibilidad tamaños

--- js/script.js
+++ js/script.js
@@ -10,6 +10,9 @@ function changenombres() {
 function changeapellidos() {
   document.getElementById("apellidoslabel").style.color = "black";
 }
+function changetamaño() {
+  document.getElementById("labeltamaño").style.color = "red";
+}
 
 document.getElementById("form"),
   addEventListener("submit", function (event) {
@@ -23,10 +26,34 @@ document.getElementById("form"),
     }
     if (document.getElementById("nombre").value != "") {
       changenombres();
-     
     }
     if (document.getElementById("apellidos").value != "") {
       changeapellidos();
-      
+    }
+    if (document.getElementById("tamaño").value == "selecciona") {
+      changetamaño();
+      event.preventDefault();
+    }
+    if (document.getElementById("tamaño").value == "S") {
+      changetamaño();
+      event.preventDefault();
     }
   });
+
+$("#tamaño").on("change", function () {
+  console.log(this.value);
+  // Aqui va la llamada AJAX con JQuery
+  if (this.value == "S") {
+    texto = "No disponible";
+
+    $("span").text(texto);
+  }
+  if (this.value != "S" && this.value != "selecciona") {
+    texto = "Disponible";
+    $("span").text(texto);
+  }
+  if (this.value == "selecciona") {
+    texto = "";
+    $("span").text(texto);
+  }
+});
